f97f6962582185ae0c40efc1576b984db0d92d16
anandspaces <anandvaibhaw000@gmail.com>
2024-03-03 23:06:42 +0530

    added icons

--- src/components/home3.js
+++ src/components/home3.js
@@ -1,9 +1,11 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function Dashboard3() {
     return (
         <div>
-            <p>write your comments<i class="bi bi-airplane-engines-fill"></i></p>
+            <p>write your comments<i className="bi bi-airplane-engines-fill"></i></p>
             <form>
                 <input type="text"></input>
             </form>
