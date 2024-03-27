b0aa3be8244f23bc8ae1bf50c0e67587dfe97b9b
anandspaces <anandvaibhaw000@gmail.com>
2024-03-27 23:06:08 +0530

    corrected dropdown

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navigation() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container">
@@ -11,38 +18,37 @@ export default function Navigation() {
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                <a className="nav-link" href="/">
-                    <i className="bi bi-house-door-fill"></i> Home
-                </a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="/profile">
-                    <i className="bi bi-person"></i> Profile
-                </a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="/message">
-                    <i className="bi bi-chat"></i> Messages
-                </a>
-                </li>
-                <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/profilesetting" role="button" data-bs-toggle="dropdown">
-                    More
-                </a>
-                <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="#settings"><i className="bi bi-gear"></i> Settings</a></li>
-                    <li><a className="dropdown-item" href="#help"><i className="bi bi-question-circle"></i> Help</a></li>
-                    <li><hr className="dropdown-divider" /></li>
-                    <li><a className="dropdown-item" href="#logout"><i className="bi bi-box-arrow-right"></i> Logout</a></li>
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/">
+                        <i className="bi bi-house-door-fill"></i> Home
+                    </Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link to="/profile" className="nav-link">
+                        <i className="bi bi-person"></i> Profile
+                    </Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link to="/message" className="nav-link">
+                        <i className="bi bi-chat"></i> Messages
+                    </Link>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <button className="nav-link dropdown-toggle" onClick={toggleDropdown} aria-expanded={isOpen ? "true" : "false"}>
+                            More
+                        </button>
+                        <ul className={`dropdown-menu${isOpen ? " show" : ""}`} aria-labelledby="navbarDropdown">
+                            <li><button className="dropdown-item">Settings</button></li>
+                            <li><button className="dropdown-item">Help</button></li>
+                            <li><button className="dropdown-item">Logout</button></li>
+                        </ul>
+                    </li>
                 </ul>
-                </li>
-            </ul>
-            <form className="d-flex ms-auto">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-primary" type="submit btn-light">Search</button>
-            </form>
+                <form className="d-flex ms-auto">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-disabled btn-light" type="submit">Search</button>
+                </form>
             </div>
         </div>
         </nav>
